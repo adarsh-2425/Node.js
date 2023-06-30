28e760aa06f17e86cf63b90081f96dfff8869ac2
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-30 14:56:44 +0530

    Create The Error Module.js

---
+++ NODE.JS ESSENTIALS/The Error Module.js
@@ -0,0 +1,8 @@
+const errorFirstCallback = (err, data)  => {
+  if (err) {
+    console.log(`There WAS an error: ${err}`);
+  } else {
+    // err was falsy
+    console.log(`There was NO error. Event data: ${data}`);
+  }
+}
