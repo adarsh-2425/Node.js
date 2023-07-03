1b054d3af75400c448161a6469597cd708301de0
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-03 17:19:13 +0530

    Update The Timers Module.js

--- NODE.JS ESSENTIALS/The Timers Module.js
+++ NODE.JS ESSENTIALS/The Timers Module.js
@@ -2,3 +2,14 @@
 setImmediate(() => {
   console.log('I got called right away!');
 })
+
+setTimeout(() => {
+  const buffer1 = Buffer.from('Lol');
+  const buffer2 = Buffer.from(' happens');
+
+  const arr = [buffer1, buffer2];
+  const join = Buffer.concat(arr);
+
+  const str = join.toString();
+  console.log(str);
+}, 3000);
