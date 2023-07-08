a21a730b054f6c7ae665682480a5a890a2804fd6
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-08 12:50:09 +0530

    Create url.format.js

---
+++ Problems/url module/url.format.js
@@ -0,0 +1,20 @@
+//Write a program that takes a hostname, a path, and a query string as input, and uses the url.format() method to create a new URL.
+
+const url = require('url');
+
+function createUrl(hostname, pathname, query) {
+  const urlObject = {
+    protocol: 'https',
+    hostname,
+    pathname,
+    query
+  };
+  return url.format(urlObject);
+};
+
+const hostname = 'www.google.com';
+const pathname = '/movies';
+const query = {key1: 'Will Smith', key2: 'Independece Day'};
+
+const newUrl = createUrl(hostname, pathname, query);
+console.log(newUrl);
