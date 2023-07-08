40bf7a6820c288cfb3472aa55753f42bf05841dd
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-08 12:54:03 +0530

    Update url.format.js

--- Problems/url module/url.format.js
+++ Problems/url module/url.format.js
@@ -17,4 +17,4 @@ const pathname = '/movies';
 const query = {key1: 'Will Smith', key2: 'Independece Day'};
 
 const newUrl = createUrl(hostname, pathname, query);
-console.log(newUrl);
+console.log(newUrl); //https://www.google.com/movies?key1=Will%20Smith&key2=Independece%20Day
