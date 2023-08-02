7a1566d1fb40930e9e5e52dc579260ff1fa73fc3
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-08-02 13:41:23 +0530

    Create create server using http.js

---
+++ create server using http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+});
+
+const port = process.env.port || 3000;
+server.listen(port, () => {
+  console.log('server running at port ${port}')
+})
